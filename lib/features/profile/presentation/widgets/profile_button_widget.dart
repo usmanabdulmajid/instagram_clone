731e396d095fce9da263001332f76ee534870167
iloveteajay <atytjani@gmail.com>
2021-07-16 22:01:18 +0100

    UI fixes and refactoring

---
+++ lib/features/profile/presentation/widgets/profile_button_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/features/profile/presentation/pages/utils/colors.dart';
+import 'package:instagram_clone/features/profile/presentation/pages/utils/sizing.dart';
+
+class ProfileButtonWidget extends StatelessWidget {
+  const ProfileButtonWidget({
+    Key key,
+    @required this.darkModeOn,
+    @required this.label,
+  }) : super(key: key);
+
+  final bool darkModeOn;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        label,
+        style: TextStyle(
+          fontFamily: 'SFProText',
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+          color: darkModeOn ? AppColors.white : AppColors.black,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        padding: EdgeInsets.symmetric(
+            vertical: 5, horizontal: Sizing.xMargin(context, 8)),
+        textStyle: TextStyle(
+          fontFamily: 'SFProText',
+          fontSize: 14,
+          fontWeight: FontWeight.normal,
+          color: darkModeOn ? AppColors.white : AppColors.black,
+        ),
+        primary: darkModeOn ? AppColors.black : AppColors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+          side: BorderSide(
+            width: 1.5,
+            color: darkModeOn ? AppColors.darkStoryGrey : AppColors.borderGrey,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/profile/presentation/widgets/profile_text_widget.dart
@@ -0,0 +1,46 @@
+
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/features/profile/presentation/pages/utils/colors.dart';
+import 'package:instagram_clone/features/profile/presentation/pages/utils/sizing.dart';
+
+class ProfileTextWidget extends StatelessWidget {
+  const ProfileTextWidget({
+    Key key,
+    @required this.darkModeOn,
+    @required this.label,
+    @required this.labelCount,
+  }) : super(key: key);
+
+  final bool darkModeOn;
+  final String label;
+  final String labelCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          labelCount,
+          style: TextStyle(
+            fontFamily: 'SFProText',
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: darkModeOn ? AppColors.white : AppColors.black,
+          ),
+        ),
+        YMargin(5),
+        Text(
+          label,
+          style: TextStyle(
+            fontFamily: 'SFProText',
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+            color: darkModeOn ? AppColors.white : AppColors.black,
+          ),
+        ),
+      ],
+    );
+  }
+}
