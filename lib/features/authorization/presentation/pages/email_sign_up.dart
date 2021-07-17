de2fae38179c3328009c21b704870f7a24f209b7
usmanabdulmajid <abstractmajid@gmail.com>
2021-07-17 04:07:09 -0700

    updated project

---
+++ lib/features/authorization/presentation/pages/email_sign_up.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/features/authorization/presentation/widgets/login_proceed_button.dart';
+
+class EmailSignUp extends StatefulWidget {
+  @override
+  _EmailSignUpState createState() => _EmailSignUpState();
+}
+
+class _EmailSignUpState extends State<EmailSignUp> {
+  TextEditingController _textEditingController;
+  FocusNode focusNode;
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    focusNode = FocusNode();
+    focusNode.unfocus();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var _size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Container(
+          width: _size.width,
+          height: 50.0,
+          margin: EdgeInsets.symmetric(horizontal: 20.0),
+          padding: EdgeInsets.only(left: 20.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6.0),
+            border: Border.all(
+              color: Colors.grey,
+            ),
+            color: Colors.grey.shade300,
+          ),
+          child: TextField(
+            onChanged: (value) {
+              setState(() {});
+            },
+            // focusNode: focusNode,
+            autofocus: false,
+            decoration: InputDecoration(
+              hintText: 'Email',
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        LoginProceedButton(
+          child: Text('Next'),
+          disableButton: true,
+        )
+      ],
+    );
+  }
+}
