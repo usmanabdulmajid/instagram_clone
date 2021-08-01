b7acb1e0c7b2e9a771782195d8c3969112893e92
usmanabdulmajid <abstractmajid@gmail.com>
2021-08-01 06:43:48 -0700

    added bottomTile on CaptureImageView

---
+++ lib/features/new_post/presentation/widgets/capture_bottom_tile.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/utils/sizing.dart';
+
+class CaptureBottomTile extends StatelessWidget {
+  final Function onTap;
+  final Widget leading;
+  final String title;
+
+  const CaptureBottomTile({this.leading, this.title, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24.0),
+        color: Colors.white12,
+      ),
+      child: Row(
+        children: [
+          Container(
+            height: 20,
+            width: 20,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+              ),
+            ),
+            child: ClipOval(child: leading),
+          ),
+          XMargin(5.0),
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 12.0,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/features/new_post/presentation/widgets/captured_image_view.dart
+++ lib/features/new_post/presentation/widgets/captured_image_view.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/core/utils/colors.dart';
+import 'package:instagram_clone/core/utils/sizing.dart';
+import 'package:instagram_clone/features/new_post/presentation/widgets/capture_bottom_tile.dart';
 
 class CapturedImageView extends StatelessWidget {
   final String imagePath;
@@ -27,9 +29,51 @@ class CapturedImageView extends StatelessWidget {
             Positioned(
               bottom: 0.0,
               child: Container(
+                padding: EdgeInsets.only(
+                  left: 10.0,
+                  right: 10.0,
+                  bottom: 15.0,
+                ),
                 width: _size.width,
                 height: 80,
                 color: AppColors.black,
+                child: Row(
+                  children: [
+                    CaptureBottomTile(
+                      onTap: () {},
+                      leading: Image.asset(
+                        'assets/images/selfie_test.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      title: 'Your Story',
+                    ),
+                    XMargin(10),
+                    CaptureBottomTile(
+                      onTap: () {},
+                      leading: Image.asset(
+                        'assets/images/selfie_test.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      title: 'Close Friends',
+                    ),
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 45,
+                        width: 45,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Icon(
+                          Icons.chevron_right,
+                          color: Colors.black,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             )
           ],
