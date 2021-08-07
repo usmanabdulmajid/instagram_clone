524057af319ede0d09918cfa812a0c3dc3b689d4
usmanabdulmajid <abstractmajid@gmail.com>
2021-08-07 06:43:25 -0700

    modify NewStoryPage

---
+++ lib/features/new_post/presentation/widgets/camera_side_menu_tile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/core/utils/sizing.dart';
+
+class CameraSideMenuTile extends StatelessWidget {
+  final bool leftSide;
+  final Widget leading;
+  final Widget title;
+  final Function onTap;
+
+  const CameraSideMenuTile(
+      {this.leftSide = true, this.leading, this.title, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    var _size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 12.0),
+        child: leftSide
+            ? Row(
+                children: [leading, XMargin(20), title],
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [title, XMargin(20), leading],
+              ),
+      ),
+    );
+  }
+}
